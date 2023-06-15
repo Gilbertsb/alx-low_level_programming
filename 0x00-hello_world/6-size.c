16a5685516814436d315e1d3c7c5dabd0ceca487
Gilbertsb <g.sibomana@alustudent.com>
2023-06-15 16:18:51 -0700

    size of datatype

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,20 +8,19 @@
  * Return: return zero
  */
 
-
 int main(void)
 {
 	char chrt;
 	int integ;
 	long lng;
-	long long dbllng;
+        long long dbllng;
 	float flt;
 
 	printf("Size of a char: %lu byte(s)\n", sizeof(chrt));
-        printf("Size of an int: %lu byte(s)\n", sizeof(integ));
-        printf("Size of a long int: %lu byte(s)\n", sizeof(lng));
-        printf("Size of a long long int: %lu byte(s)\n", sizeof(dbllng));
-        printf("Size of a float: %lu byte(s)\n", sizeof(flt));
+	printf("Size of an int: %lu byte(s)\n", sizeof(integ));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(lng));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(dbllng));
+	printf("Size of a float: %lu byte(s)\n", sizeof(flt));
 
 	return (0);
 }
